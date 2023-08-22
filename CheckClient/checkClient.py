5ff37268313dab974f9c23fcc35f8fc3e76ba12c
Nguyễn Quang Vinh <hakuno2000@gmail.com>
2023-08-22 07:16:10 +0000

    add clients data

--- CheckClient/checkClient.py
+++ CheckClient/checkClient.py
@@ -39,19 +39,21 @@ def queryHermesGetClient(chain, client):
 	dest_chain_height = dest_chain_height.replace(',', '')
 	# print(dest_chain_height)
 
+	print(dest_chain_id, dest_chain_height)
 	return dest_chain_id, dest_chain_height
 
 def queryRpcGetHeigt(rpc):
-    global miss_counter, thread_id, warned
-    header = {
-        'accept': 'application/json',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
-    }
-    try:
-        request = requests.get(f"{rpc}/block", headers=header)
-        response = request.json()
-        height = response["result"]["block"]["header"]["height"]
-        return height
+	global miss_counter, thread_id, warned
+	header = {
+		'accept': 'application/json',
+		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+	}
+	try:
+		request = requests.get(f"{rpc}/block", headers=header)
+		response = request.json()
+		height = response["result"]["block"]["header"]["height"]
+		print(height)
+		return height
 
         # if percentage > 80:
         #     if thread_id == "":
@@ -59,8 +61,8 @@ def queryRpcGetHeigt(rpc):
         #     else: 
         #         message(os.getenv("NOTI"), f"Our UMEE miss_counter: {miss_counter}, is {percentage}%!", thread_id)
         
-        warned = False
-    except Exception as e:
+		warned = False
+	except Exception as e:
         # if not warned:
         #     URL="https://hooks.slack.com/services/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"
         #     payload = {
@@ -79,11 +81,13 @@ def queryRpcGetHeigt(rpc):
         #     }
         #     requests.post(URL, data=json.dumps(payload))
         #     warned = True
-        traceback.print_exc()
+		traceback.print_exc()
             
 def checkClientState(chain, client):
 	dest_chain_id, dest_chain_height = queryHermesGetClient(chain, client)
-	# dest_chain_rpc = chains_data[dest_chain_id]["api"]
+	# dest_chain_rpc = clients_data[dest_chain_id]["api"]
+	dest_chain = clients_data[dest_chain_id]
+	print(dest_chain)
 	dest_chain_current_height = queryRpcGetHeigt('https://rpc-injective-v7dtuhp57urhpzee-ie.internalendpoints.notional.ventures:443')
 
 	block_time = 6
@@ -92,15 +96,15 @@ def checkClientState(chain, client):
 	
 	if last_update > limit:
 		if thread_id == "":
-			message(os.getenv("TEST"), f"Our {dest_chain_id} client is not updated")
+			message(os.getenv("PI"), f"Our {dest_chain_id} client is not updated")
 		else: 
-			message(os.getenv("TEST"), f"Our {dest_chain_id} client is not updated", thread_id)
+			message(os.getenv("PI"), f"Our {dest_chain_id} client is not updated", thread_id)
 
 if __name__ == "__main__":
 	# Reads chains-data file relative to the location of this file.
 	root_dir = parent_dir_name(parent_dir_name(os.path.realpath(__file__)))
-	chains_data = os.path.join(root_dir, 'chains-data.json')
-	chains_data = json.load(open(chains_data))
+	clients_data = os.path.join(root_dir, 'clients_data-data.json')
+	clients_data = json.load(open(clients_data))
 	# umee = chains_data["umee"]       
 	# umee["val"] = "umeevaloper1dmahqt84r9je3sqvljzjrttjj78cmrf39k5zhs"
 
@@ -109,4 +113,4 @@ if __name__ == "__main__":
 	#     print("Sleeping for 10 seconds\n") # block time of umee
 	#     time.sleep(10)
 
-	checkClientState('axelar-dojo-1', '07-tendermint-37')
+	checkClientState('centauri-1', '07-tendermint-1')
